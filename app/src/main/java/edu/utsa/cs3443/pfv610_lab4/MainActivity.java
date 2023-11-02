1b42e2c3446213ac505b5947e18472edce97bcd2
Mitra Bokaei Hosseini <mitra.bokaeihosseini@utsa.edu>
2023-11-02 15:48:47 -0500

    Changes to MainActivity

--- app/src/main/java/edu/utsa/cs3443/pfv610_lab4/MainActivity.java
+++ app/src/main/java/edu/utsa/cs3443/pfv610_lab4/MainActivity.java
@@ -2,13 +2,39 @@ package edu.utsa.cs3443.pfv610_lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+/*
+This class only takes care of onclick events
+that is why we can make the class to implement the interface for onclick events
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupButtons();
+    }
+
+    @Override
+    public void onClick(View view) {
+        //Do I need to send anything to the other views from this view?
+        //if button clicked is trick the go to TrickActivity
+        //else go to the treat activity
+    }
+
+    private void setupButtons(){
+        //two buttons and they both have an id which is an integer
+        int[] buttonIDs = {R.id.button1, R.id.button2};
+        String[] buttonText = {"Trick", "Treat"};
+        for(int i = 0; i < buttonIDs.length; i++){
+            Button button = findViewById(buttonIDs[i]);
+            button.setText(buttonText[i]);
+            button.setOnClickListener(this);
+        }
     }
 }
